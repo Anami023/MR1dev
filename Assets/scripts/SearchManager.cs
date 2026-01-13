fc5b18197165a47b5f45f993e013b15140f5174b
s22mrdev-admin <ananshengda@gmail.com>
2026-01-13 15:59:41 +0900

    Searchボタンをクリック後googleのブラウザ画面が表示されるようにしました。

--- Assets/scripts/SearchManager.cs
+++ Assets/scripts/SearchManager.cs
@@ -7,20 +7,29 @@ public class SearchManager : MonoBehaviour
     // インスペクターからInputFieldを紐付けます
     [SerializeField] private TMP_InputField searchInputField;
 
+    public void OpenKeyboard()
+    {
+        TouchScreenKeyboard.Open(searchInputField.text, TouchScreenKeyboardType.Default);
+    }
+
+
     /// <summary>
     /// ボタンが押された時に呼び出す関数
     /// </summary>
     public void ExecuteSearch()
     {
+        string url = "https://www.google.com/?zx=1768286493511&no_sw_cr=1";
+        Application.OpenURL(url);
         string query = searchInputField.text;
 
-        if (!string.IsNullOrEmpty(query))
+        /*if (!string.IsNullOrEmpty(query))
         {
             // 日本語やスペースをURLで使える形式（%エンコード）に変換
             string encodedQuery = Uri.EscapeDataString(query);
             
             // Google検索のURLを作成
-            string url = "https://www.google.com/search?q=" + encodedQuery;
+            
+            "https://www.google.com/search?q=" + encodedQuery;
 
             // システムブラウザを起動してURLを開く
             Application.OpenURL(url);
@@ -30,6 +39,6 @@ public class SearchManager : MonoBehaviour
         else
         {
             Debug.LogWarning("検索ワードが空です");
-        }
+        }*/
     }
 }
